056c23475a8473b1a2cdbb64680f198295a3cfd9
Stephen Soltesz <soltesz@cs.princeton.edu>
2008-11-12 17:55:27 +0000

    propagate PLC_MONITOR_HOST to the node

--- PlanetLabConf/get_plc_config.php
+++ PlanetLabConf/get_plc_config.php
@@ -34,7 +34,7 @@ echo $shebang . "\n";
 
 foreach (array('PLC_API_HOST', 'PLC_API_PATH', 'PLC_API_PORT',
 	       'PLC_WWW_HOST', 'PLC_BOOT_HOST', 'PLC_PLANETFLOW_HOST',
-	       'PLC_NAME', 'PLC_SLICE_PREFIX',
+	       'PLC_NAME', 'PLC_SLICE_PREFIX', 'PLC_MONITOR_HOST',
 	       'PLC_MAIL_SUPPORT_ADDRESS',
 	       'PLC_MAIL_MOM_LIST_ADDRESS',
 	       'PLC_MAIL_SLICE_ADDRESS')
