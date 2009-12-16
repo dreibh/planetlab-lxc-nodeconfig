b6aa898a826b4b14472c9654bce1660ca2b15571
Tony Mack <tmack@cs.princeton.edu>
2009-12-16 22:06:20 +0000

    no need to import plc_config.php

--- PlanetLabConf/sfa_config.php
+++ PlanetLabConf/sfa_config.php
@@ -5,9 +5,6 @@
 // $Id:
 //
 
-// Get admin API handle
-require_once 'plc_config.php';
-
 $config_directory = "/etc/sfa/";
 $default_name = "sfa_component_config";
 $file_name = $config_directory . $default_name;
