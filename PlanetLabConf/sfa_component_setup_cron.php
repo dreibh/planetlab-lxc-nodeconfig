97f59ae4657fcff5d32e3d73e3e33d4d7c6d4998
Tony Mack <tmack@cs.princeton.edu>
2010-02-17 21:43:34 +0000

    adding sfa-cm cron job configuration

---
+++ PlanetLabConf/sfa_component_setup_cron.php
@@ -0,0 +1,13 @@
+<?php
+//
+// installs the sfa_component_setup cron job  
+//
+// $Id:
+//
+
+$default_name = "sfa_component_setup.cron";
+$file_name = $default_name;
+readfile($file_name);
+exit();
+
+?>
