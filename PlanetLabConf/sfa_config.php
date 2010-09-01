35bac351cb0242e5727384b5134a5aac807741f2
Barış Metin <Talip-Baris.Metin@sophia.inria.fr>
2010-09-01 08:48:53 +0000

    typo

--- PlanetLabConf/sfa_config.php
+++ PlanetLabConf/sfa_config.php
@@ -8,7 +8,7 @@
 $config_directory = "/etc/sfa/";
 $default_name = "sfa_component_config";
 $file_name = $config_directory . $default_name;
-if (file_exists($filename)) {
+if (file_exists($file_name)) {
     readfile($file_name); 
 }
 exit();
