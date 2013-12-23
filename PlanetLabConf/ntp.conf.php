ee99be9498f2b373f9e2191aafb757018bf142e4
Ciro Scognamiglio <ciro.scognamiglio@cslash.net>
2013-12-23 19:07:15 +0100

    fixed ntp config

--- PlanetLabConf/ntp.conf.php
+++ PlanetLabConf/ntp.conf.php
@@ -52,9 +52,9 @@ print( "statsdir /var/log/ntpstats/\n" );
 print( "# Permit time synchronization with our time source, but do not\n");
 print( "# permit the source to query or modify the service on this system.\n");
 print( "restrict default kod nomodify notrap nopeer noquery\n");
-print( "restrict -6 default kod nomodify notrap nopeer noquery");
-print( "restrict 127.0.0.1");
-print( "restrict -6 ::1");
+print( "restrict -6 default kod nomodify notrap nopeer noquery\n");
+print( "restrict 127.0.0.1\n");
+print( "restrict -6 ::1\n");
 
 if (is_numeric(array_search($model, $problem_models))) {
   print( "tinker stepout 0\n" );
